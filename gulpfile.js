fee38cd5ff4b3c286b7fc808e4b402e6dd584ab0
Jeff Griffiths <jeff@canuckistani.ca>
2018-09-24 16:14:45 -0700

    improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,44 @@ var source = require('vinyl-source-stream');
 // metadata
 var pkgInfo = require('./package.json');
 
-gulp.task("browser", function() {
-  browserify({ 
-    debug: true
-  })
-  .transform(babelify)
-  .require('./src/bz.js', { entry: true })
-  .bundle()
-  .on("error", function (err) { console.log("Error: " + err.message); })
-  .pipe(source('bz.js'))
-  .pipe(gulp.dest('./build/browser'));
-});
+gulp.task("browser", gulp.series(function(done) {
+  browserify({
+      debug: true
+    })
+    .transform(babelify)
+    .require('./src/bz.js', {
+      entry: true
+    })
+    .bundle()
+    .on("error", function(err) {
+      console.log("Error: " + err.message);
+    })
+    .pipe(source('bz.js'))
+    .pipe(gulp.dest('./build/browser'));
+  done();
+}));
 
-gulp.task("node", function () {
+gulp.task("node", gulp.series(function(done) {
   return gulp.src(['./src/index.js', './src/xhr.js'])
-    .pipe(babel())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(gulp.dest("build/node"));
+  done();
+}));
+
+gulp.task("watch", function(done) {
+  return gulp.watch('./src/*.js', gulp.parallel(['default']));
+  done();
 });
 
-gulp.task("watch", function() {
-  return gulp.watch('./src/*.js', ['default']);
-});
+gulp.task("default", gulp.series(["browser", "node"], function(done) {
+  // copy files
+  console.log("copying files...");
+  fs.createReadStream('./build/browser/bz.js').pipe(fs.createWriteStream(
+    './test/browser/files/bz.js'));
+  fs.createReadStream('./build/browser/bz.js').pipe(fs.createWriteStream(
+    './bz-' + pkgInfo.version + '.js'));
 
-gulp.task("default", ["browser", "node"], function() {
-    // copy files
-    console.log("copying files...");
-    fs.createReadStream('./build/browser/bz.js').pipe(fs.createWriteStream('./test/browser/files/bz.js'));
-    fs.createReadStream('./build/browser/bz.js').pipe(fs.createWriteStream('./bz-'+pkgInfo.version+'.js'));
-});
+  done();
+}));
